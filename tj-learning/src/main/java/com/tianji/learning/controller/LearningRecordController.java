722d4f10547aca0746a66c373139bcebd93f3173
dream-kaii <2157497451@qq.com>
2026-04-14 12:34:11 +0800

    提交学习记录未完成..

--- tj-learning/src/main/java/com/tianji/learning/controller/LearningRecordController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/LearningRecordController.java
@@ -1,13 +1,12 @@
 package com.tianji.learning.controller;
 
 import com.tianji.api.dto.leanring.LearningLessonDTO;
+import com.tianji.learning.domain.dto.LearningRecordFormDTO;
 import com.tianji.learning.service.ILearningRecordService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "我的学习计划相关接口")
 @RequiredArgsConstructor
@@ -22,8 +21,15 @@ public class LearningRecordController {
      * @param courseId 课程id
      * @return 课表信息、学习记录及进度信息
      */
+    @ApiOperation("查询课程的学习进度")
     @GetMapping("/course/{courseId}")
     public LearningLessonDTO queryLearningRecordByCourse(@PathVariable("courseId") Long courseId){
         return learningRecordService.queryLearningRecordByCourse(courseId);
+    }
+
+    @ApiOperation("提交学习记录")
+    @PostMapping
+    public void addLearningRecord(@RequestBody LearningRecordFormDTO dto){
+
     }
 }
