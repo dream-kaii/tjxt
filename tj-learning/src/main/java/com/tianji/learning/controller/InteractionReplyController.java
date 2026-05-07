5098e84abc74d0b381c10643e894fc3a5bbcb45a
kaii <2157497451@qq.com>
2026-05-07 22:32:44 +0800

    新增功能

---
+++ tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
@@ -0,0 +1,20 @@
+package com.tianji.learning.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 互动问题的回答或评论 前端控制器
+ * </p>
+ *
+ * @author kaii
+ * @since 2026-05-05
+ */
+@RestController
+@RequestMapping("/replyes")
+public class InteractionReplyController {
+
+}
---
+++ tj-learning/src/main/java/com/tianji/learning/service/IInteractionQuestionService.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.service;
+
+import com.tianji.learning.domain.po.InteractionQuestion;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 互动提问的问题表 服务类
+ * </p>
+ *
+ * @author kaii
+ * @since 2026-05-05
+ */
+public interface IInteractionQuestionService extends IService<InteractionQuestion> {
+
+}
