5d23ca0eb4a216011e0ec56b4d75aa275a541fd3
kaii <2157497451@qq.com>
2026-05-07 23:56:22 +0800

    新增功能

--- tj-learning/src/main/java/com/tianji/learning/controller/InteractionQuestionController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/InteractionQuestionController.java
@@ -1,6 +1,12 @@
 package com.tianji.learning.controller;
 
 
+import com.tianji.learning.domain.dto.QuestionFormDTO;
+import com.tianji.learning.service.IInteractionQuestionService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2026-05-05
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/questions")
 public class InteractionQuestionController {
+    private final IInteractionQuestionService questionService;
+
+    @ApiOperation("新增提问")
+    @PostMapping
+    public void saveQuestion(QuestionFormDTO dto){
+        questionService.saveQuestion(dto);
+    }
+
+    @ApiOperation("修改提问")
+    @PutMapping("/{id}")
+    public void updateQuestion(Long id,QuestionFormDTO dto){
+        questionService.updateQuestion(id,dto);
+    }
 
 }
--- tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionQuestionServiceImpl.java
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionQuestionServiceImpl.java
@@ -1,5 +1,11 @@
 package com.tianji.learning.service.impl;
 
+import com.tianji.common.exceptions.BadRequestException;
+import com.tianji.common.exceptions.BizIllegalException;
+import com.tianji.common.utils.BeanUtils;
+import com.tianji.common.utils.StringUtils;
+import com.tianji.common.utils.UserContext;
+import com.tianji.learning.domain.dto.QuestionFormDTO;
 import com.tianji.learning.domain.po.InteractionQuestion;
 import com.tianji.learning.mapper.InteractionQuestionMapper;
 import com.tianji.learning.service.IInteractionQuestionService;
@@ -17,4 +23,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class InteractionQuestionServiceImpl extends ServiceImpl<InteractionQuestionMapper, InteractionQuestion> implements IInteractionQuestionService {
 
+    /*
+    * 保存提问
+    * */
+    @Override
+    public void saveQuestion(QuestionFormDTO dto) {
+        InteractionQuestion interactionQuestion = BeanUtils.copyBean(dto, InteractionQuestion.class);
+        Long userId = UserContext.getUser();
+        interactionQuestion.setUserId(userId);
+        //保存到数据库中
+        this.save(interactionQuestion);
+    }
+
+    @Override
+    public void updateQuestion(Long id, QuestionFormDTO dto) {
+        if(StringUtils.isBlank(dto.getTitle())||StringUtils.isBlank(dto.getDescription())||dto.getAnonymity() == null){
+            throw new BizIllegalException("非法参数!");
+        }
+        InteractionQuestion question = this.getById(id);
+        if(question==null){
+            //查不到数据
+            throw new BizIllegalException("非法参数!");
+        }
+        Long userId = UserContext.getUser();
+        if(!userId.equals(question.getUserId())){
+            //说明不是自己的问题
+            throw new BadRequestException("不能修改他人数据!");
+        }
+        question = BeanUtils.copyBean(dto, InteractionQuestion.class);
+        question.setId(id);
+        //通过plus根据id修改数据库
+        this.updateById(question);
+    }
 }
